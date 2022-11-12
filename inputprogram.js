348688bde5fa794deddaa066bd8422ce0175b53c
Robert Bubnik <86446588+rrob8@users.noreply.github.com>
2022-11-11 20:50:56 -0500

    Delete inputprogram.js

--- inputprogram.js
+++
@@ -1,6 +0,0 @@
-const prompt = require('prompt-sync')();
-var fname = prompt("Enter your first name : ");
-var lname = prompt("Enter your last name : ");
-
-console.log("your full name is",fname,lname);
-
